74e2dcccd112d360f05f8e2eaf81d3d466899e9a
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-03-27 13:22:27 +0000

    Create binary_search.py

---
+++ __pycache__/helper/binary_search.py
@@ -0,0 +1,22 @@
+def binary_search(to_search, target):  
+    """  
+    searches sorted list  
+    RETURNS: bool  
+    """  
+
+    low = 0  
+    high = len(to_search) - 1  
+    mid = 0  
+
+    while low <= high:  
+        mid = (high + low) // 2  
+
+        if to_search[mid] < target:  
+            low = mid + 1  
+        elif to_search[mid] > target:  
+            high = mid - 1
+            
+        else: #found target  
+            return True  
+
+    return False #not found target 
