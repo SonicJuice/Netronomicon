c62a0cb2bc33c4f2714f29d2685278be4fa238ea
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-03-27 14:23:32 +0000

    Create lexical_units.py

---
+++ __pycache__/keyword_extractor/lexical_units.py
@@ -0,0 +1,22 @@
+
+
+class Sentence: 
+    def __init__(self, words): 
+        """ 
+        initialises Sentence class attributes 
+        RETURNS: None 
+        """ 
+        self.words = words # list of tokens in the sentence 
+        self.stems = [] 
+        self.length = len(words) # number of tokens in the sentence 
+
+
+class Candidate: 
+    def __init__(self): 
+        """ 
+        initialises Candidate attributes 
+        RETURNS: None 
+        """ 
+        self.surface_forms = [] # candidate's surface forms 
+        self.offsets = [] # offsets of the surface form 
+        self.sentence_ids = [] # sentence id of each surface form
